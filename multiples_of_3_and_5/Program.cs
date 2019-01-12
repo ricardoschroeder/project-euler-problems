2322ecf7783b9cda115f61d59e7000c2df4a0c45
Ricardo Schroeder <ricardoschroeder@gmail.com>
2019-01-12 21:42:10 +0100

    Created solution to solve Euler Problems and the first problem (Total sum of Multiples of 3 and 5)

---
+++ multiples_of_3_and_5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace multiples_of_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Total sum of multiples of 3 and 5");
+
+            while (true)
+            {
+                Console.Write("Max number (write any letter to exit): ");
+
+                string result = Console.ReadLine();
+
+                if (int.TryParse(result, out int max))
+                {
+                    var stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    int sum = FindSum(max);
+                    stopwatch.Stop();
+                    Console.WriteLine($"Sum: {sum}. Time elapsed: {stopwatch.Elapsed}");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        static int FindSum(int max)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < max; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}

1c4cef9fe32a0e02e961b707552823eff47a62eb
Ricardo Schroeder <ricardoschroeder@gmail.com>
2019-01-13 16:16:10 +0100

    Even Fibonacci Numbers problem

---
+++ even_fibonacci_numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace even_fibonacci_numbers
+{
+    class Program
+    {
+        const int maxNumber = 4000000;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Total sum of even numbers of the Fibonacci sequence that does not exceed {maxNumber}");
+
+            int total = FibonacciEvenNumber(1, 2, 0);
+            Console.WriteLine($"Total: {total}");
+        }
+
+        static int FibonacciEvenNumber(int previousValue, int currentValue, int total)
+        {
+            int nextValue = previousValue + currentValue;
+
+            if (currentValue % 2 == 0)
+            {
+                total += currentValue;
+            }
+
+            if (nextValue <= maxNumber) 
+            {
+                return FibonacciEvenNumber(currentValue, nextValue, total);
+            }
+
+            return total;
+        }
+    }
+}
